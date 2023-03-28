99be9798127d27c877042b96b35d7f22f0cf7065
Gabriel Cloutier <gabrielcloutier2002@gmail.com>
2023-03-28 19:54:19 -0400

    folder main

---
+++ main/main.cpp
@@ -0,0 +1,40 @@
+#include "main.hpp"
+
+// INTERRUPT = PC4 ou 5
+// BLANC = PC6
+
+enum class Robot{INIT,DETECTION,TRANSMISSION};
+
+Robot detectState(){
+    Robot robot = Robot::INIT;
+    DDRC &= ~(1<<PC4) & ~(1<<PC6);
+    while (robot == Robot::INIT){
+        if (isButtonPressed(&PINC,PINC4)){ // Bouton Interrupt
+            robot = Robot::DETECTION;
+        }
+
+        else if (!(isButtonPressed(&PINC,PINC6))){ // Blanc
+            robot = Robot::TRANSMISSION;
+        }
+    }
+    return robot;
+}
+
+void executeDetectionState(){
+
+}
+
+void executeTransmissionState(){
+
+}
+
+int main(){
+    Robot robot = detectState();
+    if (robot == Robot::DETECTION){
+        executeDetectionState();
+    }
+    else{
+        executeTransmissionState();
+    }
+
+}
---
+++ main/main.hpp
@@ -0,0 +1,7 @@
+#define F_CPU 8000000UL
+
+#include <Bouton/button.h>
+#include <Led/led.hpp>
+#include <avr/io.h>
+#include <util/twi.h>
+#include <util/delay.h> 
